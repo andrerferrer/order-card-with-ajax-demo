e6f981de539c3982846522f683a342a11e564221
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-01 16:52:41 +0100

    refactor the stimulus controller (and consequences)

--- app/javascript/controllers/item_orders_controller.js
+++ app/javascript/controllers/item_orders_controller.js
@@ -2,15 +2,28 @@ import { Controller } from "stimulus"
 import { csrfToken } from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = [ "deleteBtn" ]
-
   connect() {
   }
 
-  update(event) {
+  async update(event) {
+    await this.#fetchUrl(event)
+      .then(response => response.json())
+      .then(data => {
+        this.element.outerHTML = data.newCardHTML;
+      })
+  }
+
+  async destroy(event) {
+    await this.#fetchUrl(event)
+      .then(response => {
+        if (response.ok) this.element.outerHTML = '' // if the response is ok, remove the card
+      })
+  }
+
+  #fetchUrl(event) {
     event.preventDefault(); // This is to stop the default behavior of the button
     event.stopPropagation(); // This is to stop rails on the button
-    
+
     const url = event.currentTarget.href;
     const method = event.currentTarget.dataset.method
     const options = {
@@ -20,31 +33,7 @@ export default class extends Controller {
       },
       method: method
     }
-    
-    fetch(url, options)
-      .then(response => response.json())
-      .then(data => {
-        this.element.outerHTML = data.newCardHTML;
-      })
-  }
 
-  destroy(event) {
-    event.preventDefault(); // This is to stop the default behavior of the button
-    event.stopPropagation(); // This is to stop rails on the button
-
-    const url = this.deleteBtnTarget.href;
-    const method = this.deleteBtnTarget.dataset.method
-    const options = {
-      headers: { 
-        accept: 'application/json', 
-        'X-CSRF-Token': csrfToken()
-      },
-      method: method
-    }
-
-    fetch(url, options)
-      .then(response => {
-        if (response.ok) this.element.outerHTML = '' // if the response is ok, remove the card
-      })
+    return fetch(url, options);
   }
 }
